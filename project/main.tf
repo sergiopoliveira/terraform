f250cb5fa16b6c79998e11bb5a15a68f9793c9cf
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:43:27 +0000

    added ubuntu server

--- project/main.tf
+++ project/main.tf
@@ -113,3 +113,26 @@ resource "aws_eip" "one" {
 }
 
 # 9. Create Ubuntu server and install/enable apache2
+resource "aws_instance" "web-server-instance" {
+    ami = "ami-085925f297f89fce1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = "main-key"
+
+    network_interface {
+        device_index = 0
+        network_interface_id = aws_network_interface.web-server-nic.id
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+
+    tags {
+        Name = "web-server"
+    }
+}

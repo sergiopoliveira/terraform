8d44f2ddc1e9446bfc794e308b92632fc344552d
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:44:20 +0000

    fix

--- project/main.tf
+++ project/main.tf
@@ -109,7 +109,7 @@ resource "aws_eip" "one" {
     vpc = true
     network_interface = aws_network_interface.web-server-nic.id
     associate_with_private_ip = "10.0.1.50"
-    depends_on = aws_internet_gateway.gw
+    depends_on = [aws_internet_gateway.gw]
 }
 
 # 9. Create Ubuntu server and install/enable apache2

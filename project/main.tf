ffafaaa9f084ee57c41da6605aee795e95f77f9f
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:20:31 +0000

    added route_table_association

--- project/main.tf
+++ project/main.tf
@@ -43,9 +43,18 @@ resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.prod-vpc.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
+
+    tags {
+        Name = "prod-subnet"
+    }
 }
 
 # 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.subnet-1.id
+    route_table_id = aws_route_table.prod-route-table.id
+}
+
 # 6. Create Security Group to allow port 22, 80, 443
 # 7. Create a network interface with an ip in the subnet that was created in step 4
 # 8. Assign an elastic IP (public IP address) to the network interface created in step 7

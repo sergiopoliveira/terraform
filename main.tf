8c552a478c7d7c83190fbd03488bcf93cc467497
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 14:26:13 +0000

    added ec2 instance

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 provider "aws" {
     region = "us-east-1"
-    // not recommended to use static credentials
+    # not recommended to use static credentials
     access_key = "my-access-key"
     secret_key = "my-secret-key"
 }
+
+resource "aws_instance" "my-first-server" {
+    ami = "ami-085925f297f89fce1"
+    instance_type = "t2.micro"
+}
+
+#resource "<provider>_<resource_type>" "name" {
+#    config options...
+#    key = "value"
+#    key2 = "another value"
+#}

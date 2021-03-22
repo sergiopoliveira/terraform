9bad043059cd596efba7a0c4246b033197deffb4
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:56:48 +0000

    fix port

--- project/main.tf
+++ project/main.tf
@@ -79,8 +79,8 @@ resource "aws_security_group" "allow_web" {
 
         ingress {
         description "SSH"
-        from_port = 2
-        to_port = 2
+        from_port = 22
+        to_port = 22
         protocol = "tcp"
         cidr_block = ["0.0.0.0/0"]
     }

231e62fe0d040d5f9d686c79bf74af191f4af185
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 14:37:44 +0000

    added tag

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform 
+
+terraform init
+terraform plan
+terraform apply
--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ provider "aws" {
 resource "aws_instance" "my-first-server" {
     ami = "ami-085925f297f89fce1"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "ubuntu"
+    }
 }
 
 #resource "<provider>_<resource_type>" "name" {

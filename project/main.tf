2f7c1f6551c301114b8863b19791245a4043225b
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:05:11 +0000

    fix

--- project/main.tf
+++ project/main.tf
@@ -12,5 +12,5 @@ provider "aws" {
 # 5. Associate subnet with Route Table
 # 6. Create Security Group to allow port 22, 80, 443
 # 7. Create a network interface with an ip in the subnet that was created in step 4
-# 8. Assign an elastic IP to the network interface created in step 7
+# 8. Assign an elastic IP (public IP address) to the network interface created in step 7
 # 9. Create Ubuntu server and install/enable apache2

960cbef0922126b713fe01b77b4b8546c204a9ba
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:29:25 +0000

    added security group

--- project/main.tf
+++ project/main.tf
@@ -56,6 +56,47 @@ resource "aws_route_table_association" "a" {
 }
 
 # 6. Create Security Group to allow port 22, 80, 443
+resource "aws_security_group" "allow_web" {
+    name = "allow_web_traffic"
+    description = "Allow web inbound traffic"
+    vpc_id = aws_vpc.prod-vpc.id
+
+    ingress {
+        description "HTPPS"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+
+        ingress {
+        description "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+
+        ingress {
+        description "SSH"
+        from_port = 2
+        to_port = 2
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_block = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_web"
+    }
+}
+
 # 7. Create a network interface with an ip in the subnet that was created in step 4
 # 8. Assign an elastic IP (public IP address) to the network interface created in step 7
 # 9. Create Ubuntu server and install/enable apache2

7ec0faa16a3cdf35dd0e1f76b20881eb2aa9998a
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 16:10:21 +0000

    added vpc and internet gateway

--- project/main.tf
+++ project/main.tf
@@ -6,7 +6,21 @@ provider "aws" {
 }
 
 # 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+    cidr_block = "10.0.0.0/16"
+    
+    tags {
+        Name = "production"
+    }
+}
+
 # 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.prod-vpc.id
+}
+
 # 3. Create Custom Route Table
 # 4. Create a Subnet
 # 5. Associate subnet with Route Table

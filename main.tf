c5d7c3001b0a6789467411e2d483701dafe4e52b
sergiopoliveira <sergiopoliveira@outlook.com>
2021-03-22 14:54:56 +0000

    added aws vpc and subnet

--- main.tf
+++ main.tf
@@ -14,6 +14,14 @@ resource "aws_instance" "my-first-server" {
     } 
 }
 
+resource "aws_vpc" "first-vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "production-vpc"
+    }
+}
+
 #resource "<provider>_<resource_type>" "name" {
 #    config options...
 #    key = "value"
